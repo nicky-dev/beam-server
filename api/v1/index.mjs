3ba71084b64ede9d43e9c18616693cafc8079225
nickydev <nickydev@mapboss.co.th>
2026-04-02 18:35:15 +0700

    refactor: remove unused push and stream routes and documentation

--- api/v1/index.mjs
+++ api/v1/index.mjs
@@ -1,11 +1,7 @@
 import { Router } from "express";
 import { admission } from "#api/v1/admission/index.mjs";
-import { stream } from "#api/v1/stream/index.mjs";
-import { push } from "#api/v1/push/index.mjs";
 
 const router = Router();
 router.use('/admission', admission)
-router.use('/stream', stream)
-router.use('/push', push)
 
 export const v1 = router;
